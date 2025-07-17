be47212e6e49d237db359334da016f451ddf7844
Meilany Dinda Talitha <trlybei@gmail.com>
2025-07-17 08:49:46 +0700

    fix(App): add toaster

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -4,6 +4,7 @@ import Kalkulator from "@/pages/Kalkulator"
 import Materi from "@/pages/Materi"
 import Tutorial from "@/pages/Tutorial"
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { Toaster } from "@/components/ui/sonner"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Navbar />
       <main className="p-4">
+        <Toaster/>
         <Routes>
           <Route path="/" element={<Navigate to="/kalkulator" />}></Route>
           <Route path="/kalkulator" element={<Kalkulator />} />
