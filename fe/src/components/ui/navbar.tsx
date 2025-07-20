bd7d242e143179f139490877ace738a9c4d8b3de
Meilany Dinda Talitha <trlybei@gmail.com>
2025-07-21 06:05:26 +0700

    fix(navbar): mengatur warna background dan foreground(termasuk hover, active), mengatur font, dan mengatur responsivitas pada layar kecil

--- fe/src/components/ui/navbar.tsx
+++ fe/src/components/ui/navbar.tsx
@@ -50,20 +50,20 @@ const Navbar = React.forwardRef<HTMLElement, NavbarProps>(
         className={cn(navbarVariants({ variant, sticky }), className)}
         {...props}
       >
-        <div className="container flex h-14 items-center justify-between px-4">
-          <div className="text-lg font-bold text-primary">Eliminatrix</div>
-          <div className="flex items-center gap-6">
-            <NavLink to="/kalkulator" className={({ isActive }) => cn("text-sm transition-colors", isActive ? "text-primary underline font-medium" : "text-muted-foreground hover:text-foreground")}>Kalkulator</NavLink>
-            <NavLink to="/materi" className={({ isActive }) => cn("text-sm transition-colors", isActive ? "text-primary underline font-medium" : "text-muted-foreground hover:text-foreground")}>Materi</NavLink>
-            <NavLink to="/tutorial" className={({ isActive }) => cn("text-sm transition-colors", isActive ? "text-primary underline font-medium" : "text-muted-foreground hover:text-foreground")}>Tutorial</NavLink>
+        <div className="flex h-14 items-center justify-between bg-navbar-footer-background px-6">
+          <div className="text-3xl font-text-me-one font-extrabold text-main-foreground">ELIMINATRIX</div>
+          <div className="flex items-center max-w-3/7 gap-2 text-xs md:gap-5">
+            <NavLink to="/kalkulator" className={({ isActive }) => cn("md:text-sm transition-colors", isActive ? "text-navbar-footer-foreground hover:text-nf-foreground-active underline font-medium" : "hover:text-nf-foreground-active text-navbar-footer-foreground font-medium")}>Kalkulator</NavLink>
+            <NavLink to="/materi" className={({ isActive }) => cn("md:text-sm transition-colors", isActive ? "text-navbar-footer-foreground hover:text-nf-foreground-active underline font-medium" : "hover:text-nf-foreground-active text-navbar-footer-foreground font-medium")}>Materi</NavLink>
             {mounted && (
               <Button
                 variant="default"
                 size="icon"
                 onClick={toggleTheme}
                 aria-label="Toggle Theme"
+                className="bg-navbar-footer-foreground hover:bg-nf-foreground-hover active:bg-nf-foreground-active"
               >
-                {isDark ? <Sun className="w-5 h-5 text-yellow-400" /> : <Moon className="w-5 h-5 text-blue-500" />}
+                {isDark ? <Sun className="w-5 h-5 text-mode-toggle" /> : <Moon className="w-5 h-5 text-mode-toggle" />}
               </Button>
             )}
           </div>
