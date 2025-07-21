d6ccce299774d5b96cf1db100bb2910b0e95b96d
Meilany Dinda Talitha <trlybei@gmail.com>
2025-07-22 05:39:59 +0700

    mengatur warna background footer, mengatur navbar menjadi sticky, dan menghapus route ke tutorial page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/ui/navbar"
 import { Footer } from "@/components/ui/footer" 
 import Kalkulator from "@/pages/Kalkulator"
 import Materi from "@/pages/Materi"
-import Tutorial from "@/pages/Tutorial"
+// import Tutorial from "@/pages/Tutorial"
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import { Toaster } from "@/components/ui/sonner"
 
@@ -10,17 +10,16 @@ import { Toaster } from "@/components/ui/sonner"
 function App() {
   return (
     <Router>
-      <Navbar />
-      <main className="p-4">
-        <Toaster/>
+      <Navbar sticky/>
+      <main className="p-6 flex-grow">
+        <Toaster />
         <Routes>
           <Route path="/" element={<Navigate to="/kalkulator" />}></Route>
           <Route path="/kalkulator" element={<Kalkulator />} />
           <Route path="/materi" element={<Materi />} />
-          <Route path="/tutorial" element={<Tutorial />} />
         </Routes>
       </main>
-      <Footer />
+      <Footer className="bg-navbar-footer-background"/>
     </Router>
   )
 }
