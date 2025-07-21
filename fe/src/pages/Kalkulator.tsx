1995095fc6acd3b0b55711debed746c9bf9504f2
Meilany Dinda Talitha <trlybei@gmail.com>
2025-07-22 05:37:51 +0700

    fix(Kalkulator): mengatur responsivitas height, mengatur font, menambahkan autoscroll ke result, memperbaiki toast error, memindah semua proses format matrix ke be

--- fe/src/pages/Kalkulator.tsx
+++ fe/src/pages/Kalkulator.tsx
@@ -1,9 +1,8 @@
 import { FormMatrix } from "@/components/ui/form-matrix"
 import { CalculationResult } from "@/components/ui/calculation-result"
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { toast } from "sonner"
 import { solveMatrix, type SolveMatrixRequest } from "@/lib/api"
-import { Separator } from "@/components/ui/separator"
 
 export default function Kalkulator() {
   const [result, setResult] = useState<{
@@ -11,7 +10,7 @@ export default function Kalkulator() {
     output: string
     steps: { matrix: string; desc: string }[]
   } | null>(null)
-
+  const resultRef = useRef<HTMLDivElement>(null)
   const handleCalculate = async (
     Input: SolveMatrixRequest
   ) => {
@@ -21,42 +20,40 @@ export default function Kalkulator() {
     try {
       const data = await solveMatrix(Input)
 
-      const toLatex = (m: number[][]) =>
-        "\\begin{bmatrix}" +
-        m.map((row) => {
-          const parts = [...row]
-          const last = parts.pop()!
-          return parts.join(" & ") + " &\\bigm| & " + last
-        }).join(" \\\\ ") +
-        "\\end{bmatrix}"
-
       setResult({
-        input: toLatex(Input.matrix),
-        output: toLatex(data.result),
+        input: data.input,
+        output: data.result,
         steps: data.steps.map((step: any) => ({
-          matrix: toLatex(step.matrix),
+          matrix: step.matrix,
           desc: step.desc,
         })),
       })
+      setTimeout(() => {
+        resultRef.current?.scrollIntoView({ behavior: "smooth" })
+      }, 100)
     } catch (err: any) {
-      const detail = err?.response?.data?.detail || "Terjadi kesalahan"
-      toast.error("Gagal: " + detail)
+      toast.error("Gagal", {description: err.message})
       console.error("Error:", err)
     }
   }
 
   return (
-    <div className="container py-8 space-y-12">
-      <h1 className="text-2xl font-bold text-primary">Kalkulator Eliminatrix</h1>
-
+    <div className="container md:min-h-[calc(100vh-250px)] pb-6 space-y-12 bg-main-background">
+      <div className="space-y-3">
+        <h1 className="text-2xl font-libre-franklin font-medium text-main-foreground">Kalkulator Eliminatrix</h1>
+        <p className="text-xs font-orienta text-main-foreground">Sebuah Kalkulator untuk menyelesaikan SPL dengan metode eliminasi Gauss maupun Gauss-Jordan. Membantu Anda memperoleh solusi step-by-step secara cepat, edukatif, dan ringkas.</p>
+      </div>
+      
       <FormMatrix onCalculate={handleCalculate}/>
 
-      {result && (<Separator/>) && (
-        <CalculationResult
-          input={result.input}
-          output={result.output}
-          steps={result.steps}
-        />
+      {result && (
+        <div ref={resultRef}>
+            <CalculationResult
+            input={result.input}
+            output={result.output}
+            steps={result.steps}
+          />
+        </div>
       )}
     </div>
   )
