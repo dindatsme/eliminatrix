e648e90c4efa84e4c59f25999a19e7bd706e7c2b
Meilany Dinda Talitha <trlybei@gmail.com>
2025-07-22 05:23:22 +0700

    fix(calculation-result): menengahkan tata letak serta mengatur warna dan ukuran font

--- fe/src/components/ui/calculation-result.tsx
+++ fe/src/components/ui/calculation-result.tsx
@@ -1,5 +1,6 @@
 import "katex/dist/katex.min.css"
-import { BlockMath, InlineMath } from "react-katex"
+import { BlockMath } from "react-katex"
+import { Separator } from "@/components/ui/separator"
 
 interface Step {
 	desc: string
@@ -14,20 +15,21 @@ interface Props {
 
 export function CalculationResult({ input, output, steps }: Props) {
 	return (
-		<div className="max-w-4xl mx-auto space-y-6">
-			<div>
-				<h3 className="font-semibold mb-2">Matrix Input</h3>
+		<div className="max-w-4xl md:max-w-screen mx-auto space-y-6 text-main-foreground">
+			<Separator className="bg-input-border mb-8"/>
+			<div className="text-center">
+				<h2 className="font-semibold mb-2">Matrix Input</h2>
 				<BlockMath math={input} />
 			</div>
 
-			<div>
-				<h3 className="font-semibold mb-2">Matrix Hasil</h3>
+			<div className="text-center">
+				<h2 className="font-semibold mb-2 mt-6">Matrix Hasil</h2>
 				<BlockMath math={output} />
 			</div>
 
-			<div>
-				<h3 className="font-semibold text-lg mb-2">Langkah-Langkah</h3>
-				<div className="flex flex-wrap gap-4">
+			<div className="text-center">
+				<h2 className="font-semibold mb-2 mt-6">Langkah-Langkah</h2>
+				<div className="flex flex-wrap justify-center gap-4">
 					<div className="flex flex-wrap items-center gap-2">
 						<BlockMath math={input} />
 					</div>
@@ -35,12 +37,12 @@ export function CalculationResult({ input, output, steps }: Props) {
 						const arrowCount = Math.max(4, Math.ceil(step.desc.length/2))
 						const line = "—".repeat(arrowCount)
 						return (
-						<div key={i} className="flex flex-wrap items-center gap-2">
+						<div key={i} className="flex flex-wrap items-center justify-center gap-2">
 							<div className="relative inline-block">
-								<div className="text-sm text-muted-foreground text-center">
+								<div className="text-xs text-muted-col text-center">
 									{step.desc}
 								</div>
-								<div className="text-primary">{line}{">"}</div>
+								<div className="text-muted-col text-xs">{line}{">"}</div>
 							</div>
 							<BlockMath math={step.matrix} />
 						</div>
